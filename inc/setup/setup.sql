855abda254703df634cbabc4b6125d70624221ee
Sean Burke <hawaiianchimp@gmail.com>
2012-08-16 12:05:18 -0700

    Cleaned up Setup.sql values

--- inc/setup/setup.sql
+++ inc/setup/setup.sql
@@ -47,23 +47,21 @@ CREATE TABLE IF NOT EXISTS `errors` (
   `description` mediumtext COLLATE latin1_general_ci NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=57 ;
-INSERT IGNORE INTO `pages` VALUES(37, 'index', 'Home', '0', '/index.php', 1397, 0, 'index', 'ESCan-Electronic tracking system for E-Week. In order to win prizes, attend events, and compete in competitions, you must register your UCInetID with a wristband.');
-INSERT IGNORE INTO `pages` VALUES(39, 'error', 'Error', '0', '/error.php', 848, 0, 'index', '');
-INSERT IGNORE INTO `pages` VALUES(50, 'scan', 'Scan', '', '/scan.php', 4647, 4, 'Scan', '');
-INSERT IGNORE INTO `pages` VALUES(56, 'webmaster', 'Webmaster', '', '/webmaster.php', 16, 8, 'WebMaster', '');
-INSERT IGNORE INTO `pages` VALUES(41, 'login', 'Sign In', '0', '/login.php', 1291, 0, 'login', '');
-INSERT IGNORE INTO `pages` VALUES(43, 'Sign Up', 'Sign Up', '0', '/signup.php', 1287, 0, 'Sign Up', '');
-INSERT IGNORE INTO `pages` VALUES(45, 'logout', 'Logout', '0', '/logout.php', 209, 2, '', '');
-INSERT IGNORE INTO `pages` VALUES(44, 'events', 'Events', '0', '/events.php', 1244, 0, 'events', '');
-INSERT IGNORE INTO `pages` VALUES(46, 'iforgot', 'Forgot Password', '0', '/iforgot.php', 285, 0, '', '');
-INSERT IGNORE INTO `pages` VALUES(47, 'recover', 'Recover', '0', '/recover.php', 660, 2, '', '');
-INSERT IGNORE INTO `pages` VALUES(48, 'settings', 'Settings', '0', '/settings.php', 1040, 2, '', '');
-INSERT IGNORE INTO `pages` VALUES(53, 'Register', 'Register', '', '/register.php', 4582, 0, 'Register', '');
-INSERT IGNORE INTO `pages` VALUES(49, 'admin', 'Admin', '0', '/admin.php', 3528, 6, 'admin', '');
-INSERT IGNORE INTO `pages` VALUES(52, 'statistics', 'Statistics', '', '/statistics.php', 695, 0, 'Statistics', '');
-INSERT IGNORE INTO `pages` VALUES(51, 'graph', 'Statistics', '', '/graph.php', 196, 0, 'Statistics', '');
-INSERT IGNORE INTO `pages` VALUES(55, 'instructions', 'Instructions', '', '/instructions.php', 200, 0, 'instructions', '');
-INSERT IGNORE INTO `pages` VALUES(54, 'ucinet', 'Ucinet', '', '/ucinet.php', 39, 8, '', '');
+INSERT IGNORE INTO `pages` VALUES(1, 'index', 'Home', '0', '/index.php', 0, 0, 'index', 'ESCan-Electronic tracking system for E-Week. In order to win prizes, attend events, and compete in competitions, you must register your UCInetID with a wristband.');
+INSERT IGNORE INTO `pages` VALUES(2, 'error', 'Error', '0', '/error.php', 0, 0, 'index', '');
+INSERT IGNORE INTO `pages` VALUES(3, 'scan', 'Scan', '', '/scan.php', 0, 4, 'Scan', '');
+INSERT IGNORE INTO `pages` VALUES(4, 'webmaster', 'Webmaster', '', '/webmaster.php', 0, 8, 'WebMaster', '');
+INSERT IGNORE INTO `pages` VALUES(5, 'login', 'Sign In', '0', '/login.php', 0, 0, 'login', '');
+INSERT IGNORE INTO `pages` VALUES(6, 'signup', 'Sign Up', '0', '/signup.php', 0, 0, 'Sign Up', '');
+INSERT IGNORE INTO `pages` VALUES(7, 'logout', 'Logout', '0', '/logout.php', 0, 2, '', '');
+INSERT IGNORE INTO `pages` VALUES(8, 'events', 'Events', '0', '/events.php', 0, 0, 'events', '');
+INSERT IGNORE INTO `pages` VALUES(9, 'iforgot', 'Forgot Password', '0', '/iforgot.php', 0, 0, '', '');
+INSERT IGNORE INTO `pages` VALUES(10, 'recover', 'Recover', '0', '/recover.php', 0, 2, '', '');
+INSERT IGNORE INTO `pages` VALUES(11, 'settings', 'Settings', '0', '/settings.php', 0, 2, '', '');
+INSERT IGNORE INTO `pages` VALUES(12, 'register', 'Register', '', '/register.php', 0, 0, 'Register', '');
+INSERT IGNORE INTO `pages` VALUES(13, 'admin', 'Admin', '0', '/admin.php', 0, 6, 'admin', '');
+INSERT IGNORE INTO `pages` VALUES(14, 'statistics', 'Statistics', '', '/statistics.php', 0, 0, 'Statistics', '');
+INSERT IGNORE INTO `pages` VALUES(15, 'instructions', 'Instructions', '', '/instructions.php', 0, 0, 'instructions', '');
 CREATE TABLE IF NOT EXISTS `reset` (
   `ucinetid` varchar(8) COLLATE latin1_general_ci NOT NULL,
   `secret` varchar(32) COLLATE latin1_general_ci NOT NULL,
@@ -90,12 +88,12 @@ CREATE TABLE IF NOT EXISTS `tabs` (
 INSERT IGNORE INTO `tabs` VALUES(1, 'index', 'Home', 0, 0);
 INSERT IGNORE INTO `tabs` VALUES(2, 'events', 'Events', 0, 0);
 INSERT IGNORE INTO `tabs` VALUES(3, 'login', 'Sign In', 0, 1);
-INSERT IGNORE INTO `tabs` VALUES(9, 'Register', 'Register', 0, 0);
+INSERT IGNORE INTO `tabs` VALUES(9, 'register', 'Register', 0, 0);
 INSERT IGNORE INTO `tabs` VALUES(5, 'scan', 'Scan', 3, 0);
-INSERT IGNORE INTO `tabs` VALUES(6, 'Statistics', 'Statistics', 0, 0);
+INSERT IGNORE INTO `tabs` VALUES(6, 'statistics', 'Statistics', 0, 0);
 INSERT IGNORE INTO `tabs` VALUES(7, 'admin', 'Admin', 6, 0);
 INSERT IGNORE INTO `tabs` VALUES(11, 'instructions', 'Instructions', 0, 0);
-INSERT IGNORE INTO `tabs` VALUES(12, 'WebMaster', 'Webmaster', 8, 0);
+INSERT IGNORE INTO `tabs` VALUES(12, 'webmaster', 'Webmaster', 8, 0);
 CREATE TABLE IF NOT EXISTS `users` (
   `ucinetid` varchar(8) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
   `barcode` varchar(32) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL COMMENT 'barcode id',
@@ -105,7 +103,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `level` varchar(20) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL COMMENT 'Year Level',
   `opt` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'opt-out mail',
   `access` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'access level',
-  `elig` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'prize eligible',
+  `elig` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'prize eligiblility',
   `date` date NOT NULL,
   `time` time NOT NULL,
   `volunteer` varchar(8) NOT NULL,
