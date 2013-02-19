81d8b5c98dfdcacd2583c2186f8c648e4fecc93d
Sean Burke <hawaiianchimp@gmail.com>
2013-02-19 03:47:23 -0800

    Updated to latest version
    added "Other" Major statistics, and scans now show of the date of the
    event.

--- statistics.php
+++ statistics.php
@@ -4,8 +4,8 @@ require_once 'inc/standard.php';
 //$page = new Page($name, $css);
 $page = new Page('statistics', ALL);
 $page->setTitle('Statistics');
-$page->setTab('Statistics');
-$page->startGraph(); //adds the jQuery Sparkline to the header
+$page->startGraph();
+//adds the jQuery Sparkline to the header
 
 $var_array = new VarArray();
  
@@ -26,7 +26,18 @@ $access_stat = new Statistic('pie', 'user_access', 'Registered Users - Access', 
   * Major Statistics
   */
 foreach ($var_array->getMajors() as $major) {
-	$majors[$major] = $page->DB->countOf('users', 'major = "'.$major.'"');
+	$where .= 'major NOT LIKE \''.$major.'\' AND ';
+	if($major != "Other")
+	{
+		$majors[$major] = $page->DB->countOf('users', 'major = "'.$major.'"');
+	}
+	else
+	{
+		$where = substr($where, 0, strlen($where)-5);
+		//echo "where: ".$where;
+		//$majors["other"] = $page->DB->countOf('users', $where_majors_not_like, 1);
+		$majors["Other"] = $page->DB->countOf('users', $where);
+	}
 }
 
 $majors_stat = new Statistic('pie', 'user_majors', 'Registered Users - Major', $majors, 0);//play with this number to change up the colors
@@ -63,14 +74,18 @@ $events_stat = new Statistic('bar', 'events_stat', 'Number of Events on Each Day
  */
 foreach ($var_array->getDates(false, true) as $key => $value) {
 	$new_key = date('n/d',strtotime($key));
-	$scans[$new_key] = $page->DB->countOf('scans', 'date LIKE "'.$key.'%"');
+	$sql = 'SELECT COUNT(*)
+			FROM scans
+			LEFT JOIN events 
+			ON scans.eid = events.eid
+			WHERE events.date LIKE "'.$key.'"';
+	$scans[$new_key] = $page->DB->queryUniqueValue($sql);
 	//echo 'date = "'.$key.'"';
 }
 
 $participation_stat = new Statistic('bar', 'scans_week', 'Participation on Each Day', $scans, 7);//play with this number to change up the colors
 
 /* end Events Statistic */
-
 
 
 $bottom .= $access_stat->display();
@@ -83,6 +98,12 @@ $bottom .= $events_stat->display();
 $bottom .= '<div class="separator"></div>';
 $bottom .= $participation_stat->display();
 
+
+/*
+$bottom .= '<div id="chart"></div>
+			<script src="javascript/chart.js"></script>';
+			
+*/
 
 $box = new Box('Statistics', $bottom);
 
