84ffaeef4592526e3912a6baf4efd21ea985a708
Sean Burke <seantomburke@users.noreply.github.com>
2013-09-16 19:00:34 -0400

    Fixed all website links to base

--- inc/setup/_config.php
+++ inc/setup/_config.php
@@ -6,24 +6,27 @@ function define_basic()
 define(PRODUCT,'ESCan');									//Name of Product
 define(ORGANIZATION, 'The Engineering Student Council');	//Name of Organization or Company
 define(EMAIL, 'esc.uci@email.com');							//Email seen by users when notified
-define(WEBSITE, 'http://esc.eng.uci.edu/escan/');					//url of your site where this application is hosted
-define(DESCRIPTION, 'ESCan Description');					//Description of the system
+define(DESCRIPTION, 'ESCan Description');	                //Description of the system
+
+$scriptname=end(explode('/',$_SERVER['PHP_SELF']));         //Defines the web url
+$scriptpath=str_replace($scriptname,'',$_SERVER['PHP_SELF']);
+define(WEBSITE, 'http://'.$_SERVER['SERVER_NAME'].$scriptpath);
 }
 
 
 function define_db()
 {
-define(DBDATABASE, 'c9');  			//MySQL Database Name
+define(DBDATABASE, 'c9');  			            //MySQL Database Name
 define(DBSERVER, '127.11.251.1'); 				//MySQL Server usually localhost
-define(DBUSERNAME, 'hawaiianchimp');				//MySQL Username
-define(DBPASSWORD, ''); 			//MySQL Password
+define(DBUSERNAME, 'hawaiianchimp');			//MySQL Username
+define(DBPASSWORD, ''); 			            //MySQL Password
 }
 
 function define_webmaster()
 {
-define(WEBMASTER_USERNAME, 'stburke'); 	//Enter the username of the webmaster
-define(WEBMASTER_PASSWORD, 'escan'); 	//Enter the password of the webmaster
-define(WEBMASTER_EMAIL, 'stburke@uci.edu'); 		//Enter the webmaster's email address
+define(WEBMASTER_USERNAME, 'stburke'); 	        //Enter the username of the webmaster
+define(WEBMASTER_PASSWORD, 'escan'); 	        //Enter the password of the webmaster
+define(WEBMASTER_EMAIL, 'stburke@uci.edu'); 	//Enter the webmaster's email address
 }
 
 define_db();
