b1485ad0a65611327f849478cb28392b94d2a036
Sean Burke <seantomburke@users.noreply.github.com>
2014-02-01 06:39:39 -0500

    Added top users to statistics

--- statistics.php
+++ statistics.php
@@ -12,6 +12,44 @@ $var_array = new VarArray();
  /* User Statistics 
   * 
   */
+  
+  
+//Top Participants
+
+$sql = "SELECT users.ucinetid, COUNT(*) as num_events FROM  users, barcodes, scans, events 
+        WHERE users.ucinetid = barcodes.ucinetid
+        AND barcodes.barcode = scans.barcode
+        AND scans.eid = events.eid
+        AND users.elig = 1
+        GROUP BY users.name
+        ORDER BY num_events DESC
+        LIMIT 10;";
+$page->DB->query($sql);
+
+$top_participants = '<div class="row">
+                        <h2>Top Participants</h2>
+                        <div class="legend_pie">
+                            <div class="legend_pie_inside">
+                                <div class="row center">
+            						<span class="left">User</span>
+            						<span class="right">Number of Events</span>
+            					</div>';
+
+foreach($page->DB->resultToArray() as $key => $value)
+{
+        $top_participants .=  '<div class="row">
+        						<font color="#003333">
+        						<span class="left">'.$value[0].'</span>
+        						<span class="right">'.$value[1].'</span>
+        						</font><br>
+        					    </div>';
+}
+
+$top_participants .= "      </div>
+                        </div>
+                    </div>";
+
+
 foreach($var_array->getAccess() as $key => $value)
 {
 	//echo $key.' => '.$value;
@@ -87,7 +125,8 @@ $participation_stat = new Statistic('bar', 'scans_week', 'Participation on Each 
 
 /* end Events Statistic */
 
-
+$bottom .= $top_participants;
+$bottom .= '<div class="separator"></div>';
 $bottom .= $access_stat->display();
 $bottom .= '<div class="separator"></div>';
 $bottom .= $majors_stat->display();
