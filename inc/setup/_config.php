8129d488c45cf3fa56dff9ce3c6a5bf9c9ec692a
Sean Burke <seantomburke@users.noreply.github.com>
2015-01-27 13:20:42 -0800

    returning cleardb credentials

--- inc/setup/_config.php
+++ inc/setup/_config.php
@@ -1,43 +1,43 @@
 <?php
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
 date_default_timezone_set('America/Los_Angeles');
 
 function define_basic()
 {
-define(PRODUCT,'ESCan');									//Name of Product
-define(ORGANIZATION, 'The Engineering Student Council');	//Name of Organization or Company
-define(EMAIL, 'esc.uci@email.com');							//Email seen by users when notified
-define(DESCRIPTION, 'ESCan Description');	                //Description of the system
+    define(PRODUCT,'ESCan');									//Name of Product
+    define(ORGANIZATION, 'The Engineering Student Council');	//Name of Organization or Company
+    define(EMAIL, 'esc.uci@email.com');							//Email seen by users when notified
+    define(DESCRIPTION, 'ESCan Description');	                //Description of the system
 
-$scriptname=end(explode('/',$_SERVER['PHP_SELF']));         //Defines the web url
-$scriptpath=str_replace($scriptname,'',$_SERVER['PHP_SELF']);
-define(WEBSITE, 'http://'.$_SERVER['SERVER_NAME'].$scriptpath);
+    $scriptname=end(explode('/',$_SERVER['PHP_SELF']));         //Defines the web url
+    $scriptpath=str_replace($scriptname,'',$_SERVER['PHP_SELF']);
+    define(WEBSITE, 'http://'.$_SERVER['SERVER_NAME'].$scriptpath);
 }
 
 function define_db()
 {
-    
-$url = parse_url(getenv("CLEARDB_DATABASE_URL")); //if heroku cleardb credentials are defined
-echo $url;
-    
-if($url){
-    define(DBDATABASE, substr($url["path"], 1));        //cleardb database
-    define(DBSERVER, $url["host"]); 				    //cleardb host server
-    define(DBUSERNAME, $url["user"]);			//cleardb username
-    define(DBPASSWORD, $url["pass"]);           //cleardb password
-}
-else{
-    define(DBDATABASE, 'escan');  			            //MySQL Database Name. try 'escan'
-    define(DBSERVER, '127.0.0.1'); 				    //MySQL Server. Try 'localhost' or '127.0.0.1'
-    define(DBUSERNAME, 'escan');			//MySQL Username. Try 'escan'
-    define(DBPASSWORD, ''); 			            //MySQL Password. Lookup in ESC transition files
+
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL")); //if heroku cleardb credentials are defined
+    echo $url;
+
+    if($url){
+        define(DBDATABASE, substr($url["path"], 1));        //cleardb database
+        define(DBSERVER, $url["host"]); 				    //cleardb host server
+        define(DBUSERNAME, $url["user"]);			//cleardb username
+        define(DBPASSWORD, $url["pass"]);           //cleardb password
+    }
+    else{
+        define(DBDATABASE, 'escan');  			            //MySQL Database Name. try 'escan'
+        define(DBSERVER, '127.0.0.1'); 				    //MySQL Server. Try 'localhost' or '127.0.0.1'
+        define(DBUSERNAME, 'escan');			//MySQL Username. Try 'escan'
+        define(DBPASSWORD, ''); 			            //MySQL Password. Lookup in ESC transition files
+    }
 }
 
 function define_webmaster()
 {
-define(WEBMASTER_USERNAME, 'stburke'); 	        //Enter the username of the webmaster
-define(WEBMASTER_PASSWORD, 'escan'); 	        //Enter the password of the webmaster
-define(WEBMASTER_EMAIL, 'stburke@uci.edu'); 	//Enter the webmaster's email address
+    define(WEBMASTER_USERNAME, 'stburke'); 	        //Enter the username of the webmaster
+    define(WEBMASTER_PASSWORD, 'escan'); 	        //Enter the password of the webmaster
+    define(WEBMASTER_EMAIL, 'stburke@uci.edu'); 	//Enter the webmaster's email address
 }
 
 define_db();
