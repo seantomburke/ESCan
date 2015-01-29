b9d328f5fae52de7014cd0e43f5000e66b07fee2
Sean Burke <seantomburke@users.noreply.github.com>
2015-01-28 23:19:25 -0800

    improved error table and added phpMyAdmin access

--- webmaster.php
+++ webmaster.php
@@ -23,15 +23,15 @@ $sql = 'SELECT errors.*
 $page->DB->query($sql);
 $error_array = $page->DB->resultToArray();
 	
-$table = '<table>';
+$table = '<table class="error-table">';
 $table .= '<tr>';
-$table .= '	<td>Eid</td>';
-$table .= '	<td>Message</td>';
+$table .= '	<td class="error-table-eid">Eid</td>';
+$table .= '	<td class="error-table-message">Message</td>';
 $table .= '	<td>status</td>';
 $table .= '	<td>ucinetid</td>';
 $table .= '	<td>page</td>';
 $table .= '	<td>referer</td>';
-$table .= '	<td>browser</td>';
+$table .= '	<td class="error-table-browser">browser</td>';
 $table .= '	<td>ip</td>';
 $table .= '	<td>date</td>';
 $table .= '	<td>time</td>';
@@ -56,14 +56,15 @@ foreach ($error_array as $row)
 
 $table .= '</table>';
 
-$content = 'Use phpMyAdmin to view and export all the raw data. Make sure not to modify any data unless you know what you are doing. You could end up making permanent damage that would involve reinstalling ESCan and losing data permanently. This is the raw data that runs ESCan.
-To export the data, click on the database called "'.DBDATABASE.'" and then click "Export" in the tab at the top of the page. You can then chose the "Custom" option, then chose to export the data to a file in the "Output" section.
-<br><br>
-Click below access phpMyAdmin.
-<br><br>
-<a class="" href="phpMyAdmin">phpMyAdmin</a>';
+$content = 'Click here to access 
+<a class="" href="phpMyAdmin">phpMyAdmin</a>
+<br>';
 $content .= '<br>Username: '.DBUSERNAME;
-$content .= '<br>Password: '.DBPASSWORD."<br>";
+$content .= '<br>Password: '.DBPASSWORD.'<br>
+
+<br>Use phpMyAdmin to view and export all the raw data. Make sure not to modify any data unless you know what you are doing. You could end up making permanent damage that would involve reinstalling ESCan and losing data permanently. This is the raw data that runs ESCan.
+To export the data, click on the database called "'.DBDATABASE.'" and then click "Export" in the tab at the top of the page. You can then chose the "Custom" option, then chose to export the data to a file in the "Output" section.
+<br><br>';
 	
 $user_box->setContent($content.$table);
 
