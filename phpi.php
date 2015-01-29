e996f4361fcc4d9a914f047713329ec6d8bb9236
Sean Burke <seantomburke@users.noreply.github.com>
2015-01-29 09:59:04 -0800

    trying to show error reporting

--- phpi.php
+++ phpi.php
@@ -9,7 +9,7 @@ ini_set('display_startup_errors', 1);
 ini_set('log_errors', 1); 
 ini_set('sendmail_from', 'esc.uci@gmail.com');
 ini_set('error_log', 'error_log.txt');
-error_reporting(E_ALL);
+error_reporting(-1);
 
 phpinfo();
 
